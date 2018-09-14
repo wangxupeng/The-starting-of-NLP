9f891203f18afe775b57a5f84cf26ec62a5387ac
王旭鹏 <279354244@qq.com>
2018-09-14 15:24:23 +0800

    Update cnn_bin.py

--- deep_learning_model/cnn_bin.py
+++ deep_learning_model/cnn_bin.py
@@ -1,7 +1,7 @@
 from keras.layers import *
 import sys
 
-def cnn_one(word_vec=None, kernel_size=1, filters=512):
+def cnn(word_vec=None, kernel_size=1, filters=512):
     x = word_vec
     x = SeparableConv1D(filters=filters, kernel_size=[kernel_size], strides=1,
                         padding="same", kernel_initializer="he_uniform")(x)
